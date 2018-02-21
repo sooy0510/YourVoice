f80749a2b3acf95bda029edbcd885ebfc3337911
inseonpark <inseon04@naver.com>
2018-02-21 17:25:05 +0900

    YourVoice

--- app/src/main/java/com/example/ds/yourvoice/JoinActivity.java
+++ app/src/main/java/com/example/ds/yourvoice/JoinActivity.java
@@ -1,7 +1,19 @@
 package com.example.ds.yourvoice;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by DS on 2018-02-19.
@@ -9,10 +21,97 @@ import android.support.v7.app.AppCompatActivity;
 
 public class JoinActivity extends AppCompatActivity {
 
+    private EditText editTextId;
+    private EditText editTextPw;
+    private EditText editTextName;
+    private EditText editTextPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
+
+        editTextId = (EditText)findViewById(R.id.newID);
+        editTextPw = (EditText)findViewById(R.id.newPW);
+        editTextName = (EditText)findViewById(R.id.newName);
+        editTextPhone = (EditText)findViewById(R.id.newPhone);
     }
 
+    public void insert(View v) {
+        String Id = editTextId.getText().toString();
+        String Pw = editTextPw.getText().toString();
+        String Name = editTextName.getText().toString();
+        String Phone = editTextPhone.getText().toString();
+
+        insertToDatabase(Id, Pw, Name, Phone);
+    }
+
+    private void insertToDatabase(String Id, String Pw, String Name, String Phone) {
+        class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog loading;
+        @Override
+            protected void onPreExecute() {
+            super.onPreExecute();
+            loading = ProgressDialog.show(JoinActivity.this, "Please Wait", null, true, true);
+        }
+
+        @Override
+            protected void  onPostExecute(String s) {
+            super.onPostExecute(s);
+            loading.dismiss();
+            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG). show();
+        }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    String Id = (String) params[0];
+                    String Pw = (String) params[1];
+                    String Name = (String) params[2];
+                    String Phone = (String) params[3];
+
+
+                    String link = "http://203.252.219.238/post.php";
+                    String data = URLEncoder.encode("Id", "UTF-8") + "=" + URLEncoder.encode(Id, "UTF-8");
+                    data += "&" + URLEncoder.encode("Pw", "UTF-8") + "=" + URLEncoder.encode(Pw, "UTF-8");
+                    data += "&" + URLEncoder.encode("Name", "UTF-8") + "=" + URLEncoder.encode(Name, "UTF-8");
+                    data += "&" + URLEncoder.encode("Phone", "UTF-8") + "=" + URLEncoder.encode(Phone, "UTF-8");
+
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+
+                    wr.write(data);
+                    wr.flush();
+
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                } catch (Exception e) {
+                    return new String("Exception: " + e.getMessage());
+                }
+            }
+        }
+        InsertData task = new InsertData();
+        task.execute(Id, Pw, Name, Phone);
+
+
+    }
 }

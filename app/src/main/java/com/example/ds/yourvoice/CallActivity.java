7e649c5bf8535fb45b70129bdc9373b1eb83216c
inseonpark <inseon04@naver.com>
2018-03-15 18:47:46 +0900

    YourVoice // 0315

---
+++ app/src/main/java/com/example/ds/yourvoice/CallActivity.java
@@ -0,0 +1,47 @@
+package com.example.ds.yourvoice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.vidyo.VidyoClient.Connector.ConnectorPkg;
+import com.vidyo.VidyoClient.Connector.Connector;
+
+public class CallActivity extends AppCompatActivity implements Connector.IConnect {
+
+    private Connector vc;
+    private FrameLayout videoFrame;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.call);
+
+        ConnectorPkg.setApplicationUIContext(this);
+        ConnectorPkg.initialize();
+        videoFrame = (FrameLayout)findViewById(R.id.videoFrame);
+    }
+
+    public void Start(View v) {
+        vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 15, "warning info@VidyoClient info@VidyoConnector", "", 0);
+        vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
+    }
+
+    public void Connect(View v) {
+        String token = "cHJvdmlzaW9uAHVzZXIxQDQ5M2Y3ZS52aWR5by5pbwA2MzY4ODMxODc1MAAAMGEwMzFlYmY5ZjlkMGQ4ZTJlNjFmNTYyMzM3OGQ3N2JhMjViMjQ5OTUzN2ZiYmM1NDAyYjM2MzM3ZTc0ZDA4NWEzOTJlYzFmNzQ2MDZkMWIwZTBlM2Q5OTU4MWQ3ZDMy";
+        vc.connect("prod.vidyo.io", token, "DemoUser", "DemoRoom", this);
+        vc.c
+    }
+
+    public void Disconnect(View v) {
+        vc.disconnect();
+    }
+
+    public void onSuccess() {}
+
+    public void onFailure(Connector.ConnectorFailReason reason) {}
+
+    public void onDisconnected(Connector.ConnectorDisconnectReason reason) {}
+}
